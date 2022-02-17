84b1a4deec602da89911e9b301de8dbaf68d0a16
programVeins <sabesh5601@gmail.com>
2022-02-17 11:18:44 +0530

    Initial Commit

---
+++ toolbarTest/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  toolbarTest
+//
+//  Created by Sabesh Bharathi on 17/02/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
